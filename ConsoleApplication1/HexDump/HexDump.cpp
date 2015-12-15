c9bd7b17e6a4073bb9943ea8ea672b5cf4e8ad7e
Tom Lazar <16lazart@elmbrookstudents.org>
2015-12-15 08:48:52 -0600

    Changed file to get intire text no print it out one at a time

--- ConsoleApplication1/HexDump/HexDump.cpp
+++ ConsoleApplication1/HexDump/HexDump.cpp
@@ -7,16 +7,18 @@ using namespace std;
 int main()
 {
 	string line;
+	string text;
 	ifstream mfile;
 	mfile.open("HexDump.cpp");
 	if (mfile.is_open()) {
-		cout << "File Opened" << endl;
 		while ( getline(mfile, line)) {
-			cout << line << endl;
+			text += line;
+			text += '\n';
 		}
 		mfile.close();
 	}
 	else cout << "File Not Found " << endl;
+	cout << text << endl;
 	while (true);
     return 0;
 }
