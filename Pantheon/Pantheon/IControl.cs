b2c50649ab251e3f503ff205e48a72e1e6924f43
Tom Lazar <16lazart@elmbrookstudents.org>
2016-03-25 16:36:43 -0500

    Added a new Project and started creating the basics of an IControl System

---
+++ Pantheon/Pantheon/IControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantheon
+{
+    public abstract class IControl
+    {
+        public abstract void TeleopInit();
+
+        public abstract void TeleopPeriodic();
+
+        public abstract void AutoInit();
+
+        public abstract void AutoPeriodic();
+    }
+}
---
+++ Pantheon/Pantheon/Pantheon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WPILib;
+using WPILib.SmartDashboard;
+
+namespace Pantheon
+{
+    /// <summary>
+    /// The VM is configured to automatically run this class, and to call the
+    /// functions corresponding to each mode, as described in the IterativeRobot
+    /// documentation. 
+    /// </summary>
+    public class Pantheon : IterativeRobot
+    {
+
+        public override void RobotInit()
+        {
+           
+        }
+
+       
+        public override void AutonomousInit()
+        {
+
+        }
+
+        /// <summary>
+        /// This function is called periodically during autonomous
+        /// </summary>
+        public override void AutonomousPeriodic()
+        {
+
+        }
+
+        /// <summary>
+        /// This function is called periodically during operator control
+        /// </summary>
+        public override void TeleopPeriodic()
+        {
+
+        }
+
+        /// <summary>
+        /// This function is called periodically during test mode
+        /// </summary>
+        public override void TestPeriodic()
+        {
+
+        }
+    }
+}
