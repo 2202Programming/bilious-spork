ead1dc57532143c7aa73580a6c33707308d70015
Tom Lazar <thomas.lazar@marquette.edu>
2016-08-02 16:51:23 -0500

    Added new Service Contract Sln
    Added the 2202 Service to the Pantheon Directory, and started trying to add clients to the SmartWriter, and robot Client for right now until I create a full app, Though I think I have to beacuase IIS is shit.

---
+++ Pantheon/FRC2202/App_Code/ReportingService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+[ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+public class ReportingService : IReportingService
+{
+    /// <summary>
+    /// The data
+    /// </summary>
+    Dictionary<string, string> Data = new Dictionary<string, string>();
+
+    /// <summary>
+    /// Gets the data.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public string GetData(string key)
+    {
+        return Data[key];
+    }
+
+    /// <summary>
+    /// Gets the data cache.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public Dictionary<string, string> GetAllData()
+    {
+        return Data;
+    }
+
+    /// <summary>
+    /// Reports the data.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="value">The value.</param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void ReportData(string key, string value)
+    {
+        var temp = Data[key];
+        if (temp == null)
+            Data.Add(key, value);
+        else
+            temp = value;
+    }
+}
