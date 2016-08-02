6fb70f90cb0105793cd16f57a4f9055d0500cbc0
Tom Lazar <thomas.lazar@marquette.edu>
2016-08-01 20:58:47 -0500

    MovednSome Stuff

---
+++ Pantheon/Robot/Robot1/Robot1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using WPILib;
+using WPILib.Extras.AttributedCommandModel;
+
+namespace Robot1
+{
+    public class Robot1 : AttributedRobot
+    {
+        // You don't need to add any code here unless AttributedRobot can't do something that you need to do.
+        // By default, you can leave this empty.
+    }
+}
